dca909d9ea1c520030bdb3d4c63d055168fd3c88
Filip Tomaszewski <zambari@gmail.com>
2018-07-08 00:01:14 +0200

    Destroy on trigger

---
+++ Helpers/DestoryOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class DestoryOnTrigger : MonoBehaviour
+{
+    [ReadOnly]
+    [SerializeField] int objectsDestroyed;
+    void Reset()
+    {
+        var v = GetComponent<BoxCollider>();
+        v.isTrigger = true;
+		if (GetComponent<MeshRenderer>()!=null) GetComponent<MeshRenderer>().enabled=false;
+		name="Destructor";
+    }
+    void OnTriggerEnter(Collider c)
+    {
+
+        Destroy(c.gameObject);
+        objectsDestroyed++;
+    }
+}
