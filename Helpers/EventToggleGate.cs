2920739811585b246f6d91ee854c3529e7873d74
Filip Tomaszewski <zambari@gmail.com>
2018-12-01 20:42:37 +0100

    toggle gate added (probably not the latest)

---
+++ Helpers/EventToggleGate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class EventToggleGate : MonoBehaviour {
+
+	// Use this for initialization
+	Toggle toggle;
+	public VoidEvent whenToggledOn;
+	public VoidEvent whenToggledOff;
+	public BoolEvent invertedTrigger;
+
+	void Start () {
+		 toggle=GetComponent<Toggle>();
+		 toggle.onValueChanged.AddListener(OnToggleValue);
+	}
+
+	void OnToggleValue(bool b)
+	{
+		if (!enabled) return;
+		if (b) whenToggledOn.Invoke();
+		else whenToggledOff.Invoke();
+		invertedTrigger.Invoke(!b);
+
+	}
+	
+}
