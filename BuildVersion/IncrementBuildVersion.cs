f4029c913bda52cc60d5104cd9705dfec11d5f7a
zambari <zambari@gmail.com>
2019-04-15 16:16:33 +0200

    Build version bugfixes

---
+++ BuildVersion/IncrementBuildVersion.cs
@@ -0,0 +1,37 @@
+﻿// zambari
+// v.0.2 
+// v.0.3 - moved data class to showbuildver
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+using System.Collections.Generic;
+namespace Z
+{
+
+    public class IncrementBuildVersion : ScriptableObject
+    {
+
+        [PostProcessBuild]
+        public static void OnPostProcessBuild(BuildTarget target, string buildPath)
+        {
+            var b = new ShowBuildVersion.BuildVersion();
+            try
+            {
+                b = b.FromJson("buildInfo.json");
+                if (b == null) b = new ShowBuildVersion.BuildVersion();
+                b.buildNr++;
+            }
+            catch
+            {
+                Debug.Log("buildInfo.json created in streaminAssets");
+            }
+            b.buildDate = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            if (b.versionHistory == null) b.versionHistory = new List<string>();
+            b.versionHistory.Insert(0, b.ToString());
+            b.ToJson("buildInfo.json");
+            Debug.Log("UpdatedBuildVersion To " + b.buildNr);
+        }
+    }
+}
+#endif
---
+++ BuildVersion/ShowBuildVersion.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Z;
+[RequireComponent(typeof(Text))]
+public class ShowBuildVersion : MonoBehaviour
+{
+     [System.Serializable]
+    public class BuildVersion
+    {
+        public int buildNr = 1;
+        public string buildDate;
+        public List<string> versionHistory;
+        public override string ToString()
+        {
+            return "time: "+buildDate+"   build nr: "+buildNr;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GetMac());
+    }
+
+    IEnumerator GetMac()
+    {
+
+        Text text = GetComponent<Text>();
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+        string url = "jar:file://" + Application.dataPath + "!/assets/" + "buildInfo.json";
+#else
+        string url = "file://" + System.IO.Path.Combine(Application.streamingAssetsPath, "buildInfo.json");
+#endif
+             Debug.Log("reading buildver from   " + url);
+        var www = new WWW(url);
+        yield return www;
+        text.text = "Build unknown";
+        if (www.error != null)
+        {
+
+      Debug.Log("error " + www.error);
+        }
+        else
+        {
+            var buildVersion = JsonUtility.FromJson<ShowBuildVersion.BuildVersion>(www.text);
+            if (buildVersion != null)
+                text.text = "Build " + buildVersion.buildNr;
+        }
+    }
+}
