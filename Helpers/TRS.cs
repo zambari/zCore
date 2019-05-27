cfbd10e7a30deefd52c387dd9fff183b1694718e
zambari <zambari@gmail.com>
2019-05-27 16:13:54 +0200

    Grid creator, Transform saver utils
    for baking transform states resulting from physics simulation

---
+++ Helpers/TRS.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace Z
+{
+    // v.0.2, apply method, transform taking constructor
+
+    /// <summary>
+    /// This class is container for position rotation and scale
+    /// </summary>
+
+
+    [System.Serializable]
+    public struct TRS
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 scale;
+        public TRS(Vector3 pos, Quaternion rot, Vector3 sca)
+        {
+            scale = sca;
+            rotation = rot;
+            position = pos;
+        }
+        public TRS(Transform transform)
+        {
+            scale=transform.localScale;
+            rotation=transform.localRotation;
+            position=transform.localPosition;
+        
+        }
+         public TRS(Vector3 pos, Vector3 sca)
+        {
+            position = pos;
+            rotation = Quaternion.identity;
+            scale = sca;
+        }
+        public void Apply(Transform t)
+        {
+            t.localPosition=position;
+            t.localRotation=rotation;
+            t.localScale=scale;
+        }
+      
+        public Matrix4x4 GetMatrix()
+        {
+            return Matrix4x4.TRS(position, rotation, scale);
+        }
+    }
+}
---
+++ Helpers/TransformSaver.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Z
+{
+
+/// <summary>
+/// Purpose of this class is to save outcom of physics simulation. Run your simulation and press Save, 
+/// when you exit play mode, press Load and Remove Rigidbodies - you now have a baked final state of yoru obejcts
+/// </summary>
+
+    public class TransformSaver : MonoBehaviour
+    {
+        [System.Serializable]
+        public class TRSContainer
+        {
+            public TRS[] transforms;
+        }
+        TRSContainer transforms;
+        public string fileName = "transformSave";
+        public bool removeRigidBodies=true;
+
+        [ExposeMethodInEditor]
+        public void Save()
+        {
+            transforms = new TRSContainer();
+            transforms.transforms = new TRS[transform.childCount];
+            for (int i = 0; i < transforms.transforms.Length; i++)
+            {
+                transforms.transforms[i] = new TRS(transform.GetChild(i));
+            }
+            transforms.ToJson(fileName);
+
+        }
+        [ExposeMethodInEditor]
+        public void Load()
+        {
+            transforms = transforms.FromJson(fileName);
+            if (transforms.transforms.Length != transform.childCount)
+            {
+                Debug.Log($"invalid child count, should be {transforms.transforms.Length}, is {transform.childCount} (sure this is the right object?");
+                return;
+            }
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Undo.RegisterCompleteObjectUndo(transform.GetChild(i),"Loaded Saved Transforms");
+               transforms.transforms[i].Apply(transform.GetChild(i));
+
+               if (removeRigidBodies)
+               {
+                   var rb=transform.GetChild(i).GetComponent<Rigidbody>();
+                   if (rb!=null) DestroyImmediate(rb);
+               }
+
+            }
+        }
+    }
+}
+#endif
