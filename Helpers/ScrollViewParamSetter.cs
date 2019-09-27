b6973c2d2799af2eab73d44f367cc45019571119
zambari <zambari@gmail.com>
2019-09-27 15:27:32 +0200

    scroll parem forgotten update

--- Helpers/ScrollViewParamSetter.cs
+++ Helpers/ScrollViewParamSetter.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 namespace zUI
 {
+
+	//v .02
 #if UNITY_EDITOR
     public class ScrollViewParamSetter : MonoBehaviour
     {
-        void Reset()
+        [ExposeMethodInEditor]
+        void Doit_Destructive()
         {
             var scroll = GetComponent<ScrollRect>();
+            if (scroll == null)
+            {
+                Debug.Log("Please create a Scrollview / ScrollRect manually");
+            }
             scroll.horizontal = false;
             DestroyImmediate(scroll.horizontalScrollbar.gameObject);
             var vertScroll = scroll.verticalScrollbar;
+			scroll.GetComponent<Image>().color=Color.black*0.5f;
             vertScroll.handleRect.GetComponent<Image>().sprite = null;
             vertScroll.GetComponent<Image>().sprite = null;
             vertScroll.GetComponent<RectTransform>().sizeDelta = new Vector2(10, 0);
-            vertScroll.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -10);
+          //  vertScroll.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -10);
             var sliding = scroll.verticalScrollbar.transform.Find("Sliding Area");
             if (sliding == null) Debug.Log("no slinding area"); else sliding.GetComponent<RectTransform>().sizeDelta = Vector2.zero;
             sliding.Find("Handle").GetComponent<RectTransform>().sizeDelta = Vector2.zero;
@@ -26,7 +34,7 @@ namespace zUI
             layout.SetChildControl();
             UnityEditor.Selection.activeGameObject = layout.gameObject;
             scroll.content.gameObject.AddTextChild("content");
-            DestroyImmediate(this);
+            UnityEditor.EditorApplication.delayCall += () => DestroyImmediate(this);
         }
     }
 #endif
