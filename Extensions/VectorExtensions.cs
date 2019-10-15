7631403ef154adaa5592a4eb150a94e5bc350f44
Filip Tomaszewski <zambari@gmail.com>
2019-10-15 22:05:43 +0200

    Vector2 squared

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 // zambari 2019
 namespace Z
 {
+    // v.02 squared, formatting
+
     public static class VectorExtensions
     {
         public static Vector2 DeadZone(this Vector2 v, float zone, out bool wasOutside)
@@ -55,8 +57,6 @@ namespace Z
             {
                 v.x -= zone;
                 if (v.x < 0) v.x = 0;
-
-
             }
             else
             {
@@ -64,32 +64,44 @@ namespace Z
                 if (v.x > 0) v.x = 0;
             }
 
-
             if (v.y > 0)
             {
                 v.y -= zone;
                 if (v.y < 0) v.y = 0;
-
-
             }
             else
             {
                 v.y += zone;
                 if (v.y > 0) v.y = 0;
             }
-
-
             return v;
-
         }
 
+        public static Vector2 Squared(this Vector2 v)
+        {
+            float signx=1;
+            float signy=1;
+            if (v.x<0) signx=-1;
+            if (v.y<0) signy=-1;
+            return new Vector2(v.x*v.x*signx,v.y*v.y*signy);
+        }
+
+        public static Vector3 Squared(this Vector3 v)
+        {
+            float signx=1;
+            float signy=1;
+            float signz=1;
+            if (v.x<0) signx=-1;
+            if (v.y<0) signy=-1;
+            if (v.z<0) signz=-1;
+            return new Vector3(v.x*v.x*signx,v.y*v.y*signy,v.z*v.z*signz);
+        }
         public static Vector3 DeadZone(this Vector3 v, float zone)
         {
             if (v.x > 0)
             {
                 v.x -= zone;
                 if (v.x < 0) v.x = 0;
-
             }
             else
             {
@@ -97,13 +109,10 @@ namespace Z
                 if (v.x > 0) v.x = 0;
             }
 
-
             if (v.y > 0)
             {
                 v.y -= zone;
                 if (v.y < 0) v.y = 0;
-
-
             }
             else
             {
@@ -115,7 +124,6 @@ namespace Z
             {
                 v.z -= zone;
                 if (v.z < 0) v.z = 0;
-
             }
             else
             {
