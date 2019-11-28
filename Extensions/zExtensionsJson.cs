05300e3ba30c7bd8bcc2b04d5bb32ee2ed700fd9
Filip Tomaszewski <zambari@gmail.com>
2019-11-28 20:42:04 +0100

    json cleanup, silent mode

--- Extensions/zExtensionsJson.cs
+++ Extensions/zExtensionsJson.cs
@@ -9,44 +9,60 @@ using UnityEditor;
 
 /// oeverrides zRectExtensions
 
+// v .3 silent mode
+
 public static class zExtensionsJson
 {
-    public static void ToJson(this object obj, string path) // different naming conventino
-    {
-        if (!Directory.Exists(Application.streamingAssetsPath)) Directory.CreateDirectory(Application.streamingAssetsPath);
-        obj.SaveJson(Application.streamingAssetsPath + "/" + path);
-    }
-
-    /// <summary>
+      /// <summary>
     ///  Saves this object as Json file
     /// </summary>
     /// 
-    public static void SaveJson(this object obj, string path)
+
+
+    public static void ToJson(this object obj, string path, bool silent = false) // different naming conventino
     {
+        if (!Directory.Exists(Application.streamingAssetsPath)) Directory.CreateDirectory(Application.streamingAssetsPath);
+        // obj.SaveJson(Application.streamingAssetsPath + "/" + path);
         string dataAsJson = JsonUtility.ToJson(obj, true);
         if (!path.Contains(".json")) path += ".json";
         File.WriteAllText(path, dataAsJson);
         if (File.Exists(path))
         {
-            Debug.Log("saved : " + path);
+            if (!silent)
+            {
+                Debug.Log("saved : " + path);
 #if UNITY_EDITOR
-       //     AssetDatabase.Refresh();
+                AssetDatabase.Refresh();
 #endif
+            }
         }
         else
             Debug.Log("saving failed, file not created : " + path);
+
     }
+
+  
 
     /// <summary>
     /// Loads an object from json. usage: newObject= newObject.FromJson &lt;typeOfNewObject&gt;(path)
     /// this version adds streamingAssetsPath to path string
     /// </summary>
-    public static T FromJson<T>(this T obj, string path) // different naming conventino
+    public static T FromJson<T>(this T obj, string path, bool silent = false) // different naming conventino
     {
 
         if (!path.Contains(".json")) path += ".json";
         if (!path.Contains(Application.streamingAssetsPath)) path = Application.streamingAssetsPath + "/" + path;
-        return obj.LoadJson<T>(path);
+        if (!File.Exists(path)) return default(T);
+        string dataAsJson = File.ReadAllText(path);
+
+        if (dataAsJson == null || dataAsJson.Length < 2)
+        {
+            if (!silent)
+                Debug.Log("loading file:" + path + " failed");
+        }
+        else
+            obj = JsonUtility.FromJson<T>(dataAsJson);
+        return obj;
     }
 
 
@@ -54,21 +70,6 @@ public static class zExtensionsJson
     /// <summary>
     /// Loads an object from json. usage: newObject= newObject.FromJson&lt;typeOfNewObject&gt;(path)
     /// </summary>
-    public static T LoadJson<T>(this T obj, string path)
-    {
-        //   if (!path.Contains(".json")) path+=".json";
-        //   if (!path.Contains(Application.streamingAssetsPath)) path = Application.streamingAssetsPath+"/"+path;
-        if (!File.Exists(path)) return default(T);
-        string dataAsJson = File.ReadAllText(path);
-        if (dataAsJson == null || dataAsJson.Length < 2)
-            Debug.Log("loading file:" + path + " failed");
-        else
-            obj = JsonUtility.FromJson<T>(dataAsJson);
-        return obj;
-    }
-    [Obsolete]
-    public static T loadJson<T>(this T obj, string path)
-    {
-        return LoadJson(obj, path);
-    }
+
+
 }
